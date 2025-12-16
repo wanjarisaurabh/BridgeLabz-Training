de5e274d2944946980b4cc04d5fd3dc0f6d9b3d8
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-17 01:42:46 +0530

    [saurabh wanjari] Add. Added file AreaOfCircle

---
+++ gcr-codebase/program-prerequisite/AreaOfCircle.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle = " + area);
+
+        sc.close();
+    }
+}
