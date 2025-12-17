1fc328d3d4c899f2b2aeadab666e61fb1e75cf84
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 01:28:17 +0530

    [saurabh wanjari ] Add . Added file TriangleArea

---
+++ gcr-codebase/programming-elements/level-2/TriangleArea.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class TriangleArea {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //input
+        System.out.print("Enter base in cm: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height in cm: ");
+        double height = sc.nextDouble();
+
+        //logic code
+        double areaCm = 0.5 * base * height;
+        double areaIn = areaCm / (2.54 * 2.54);
+
+        //output
+        System.out.println("The Area of the triangle in sq in is " + areaIn + " and sq cm is " + areaCm);
+    }
+}
