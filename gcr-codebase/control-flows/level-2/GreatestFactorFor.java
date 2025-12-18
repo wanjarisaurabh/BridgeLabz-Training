6c54dca0337b3a921cef37d5408ddb98a95ebde2
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 23:13:27 +0530

    [saurabh wanjari ] Add . Added file GreatestFactorFor

---
+++ gcr-codebase/control-flows/level-2/GreatestFactorFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class GreatestFactorFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
