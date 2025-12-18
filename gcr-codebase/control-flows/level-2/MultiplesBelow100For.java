541f14fecee24cc9a5eed5ae36e1e04462388316
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 23:13:27 +0530

    [saurabh wanjari ] Add . Added file MultiplesBelow100For

---
+++ gcr-codebase/control-flows/level-2/MultiplesBelow100For.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MultiplesBelow100For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i = 100; i >= 1; i--)
+            if (i % number == 0)
+                System.out.println(i);
+    }
+}
