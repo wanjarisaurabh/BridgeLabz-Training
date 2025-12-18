ab20e981f0efb2bf16161b08f175c2c754da2d7f
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 22:49:09 +0530

    [saurabh wanjari] Add . Added file FirstSmallest program

---
+++ gcr-codebase/control-flows/level-1/FirstSmallest.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class FirstSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+
+        if (number1 < number2 && number1 < number3) {
+            System.out.println("Is the first number the smallest? Yes");
+        } else {
+            System.out.println("Is the first number the smallest? No");
+        }
+    }
+}
