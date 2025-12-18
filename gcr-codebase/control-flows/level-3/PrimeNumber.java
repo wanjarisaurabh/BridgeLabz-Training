53d1a833ac37bdfc2660d5760b21ab1b2bb24053
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 23:15:39 +0530

    [saurabh wanjari ] Add . Added file PrimeNumber

---
+++ gcr-codebase/control-flows/level-3/PrimeNumber.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        if (n <= 1) isPrime = false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime ? "Prime Number" : "Not a Prime Number");
+    }
+}
