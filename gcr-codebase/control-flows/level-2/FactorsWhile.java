de92fd9bff72bc4fdfa769b59e378e81f3bb0636
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 23:13:25 +0530

    [saurabh wanjari ] Add . Added file FactorsWhile

---
+++ gcr-codebase/control-flows/level-2/FactorsWhile.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int i = 1;
+        while (i < number) {
+            if (number % i == 0)
+                System.out.println(i);
+            i++;
+        }
+    }
+}
