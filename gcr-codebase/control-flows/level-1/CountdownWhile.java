859029c66dfac22e72ee351bc577879534341660
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-18 22:38:44 +0530

    [saurabh wanjari] Add . Added file DivisibleByFive

---
+++ gcr-codebase/control-flows/level-1/CountdownWhile.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class CountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = sc.nextInt();
+
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+}
---
+++ gcr-codebase/control-flows/level-1/DivisibleByFive.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+    }
+}
