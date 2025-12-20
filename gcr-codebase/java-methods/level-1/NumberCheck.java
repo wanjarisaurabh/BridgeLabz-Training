9fdd6d543e682c2eef329f872c22e5f27b6e59b7
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-21 01:32:04 +0530

    [saurabh wanjari] Add . Added file NumberCheck

---
+++ gcr-codebase/java-methods/level-1/NumberCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+
+    // Method to check number
+    public static int checkNumber(int n) {
+        if (n > 0)
+            return 1;
+        else if (n < 0)
+            return -1;
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("Result: " + checkNumber(n));
+    }
+}
