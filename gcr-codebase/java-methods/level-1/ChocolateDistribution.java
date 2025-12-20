f28dccc0092b42b715ba1a85ec6a961f4fce4680
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-21 01:32:04 +0530

    [saurabh wanjari] Add . Added file ChocolateDistribution

---
+++ gcr-codebase/java-methods/level-1/ChocolateDistribution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ChocolateDistribution {
+
+    // Method to distribute chocolates
+    public static int[] findRemainderAndQuotient(int chocolates, int children) {
+        return new int[]{chocolates / children, chocolates % children};
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(chocolates, children);
+
+        System.out.println("Each child gets: " + result[0]);
+        System.out.println("Remaining chocolates: " + result[1]);
+    }
+}
