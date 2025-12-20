a6981847c26fe8848da82ce0dc29243fb4539959
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-21 01:32:05 +0530

    [saurabh wanjari] Add . Added file SpringSeason

---
+++ gcr-codebase/java-methods/level-1/SpringSeason.java
@@ -0,0 +1,21 @@
+public class SpringSeason {
+
+    // Method to check spring season
+    public static boolean isSpringSeason(int month, int day) {
+        return (month == 3 && day >= 20) ||
+               (month == 4) ||
+               (month == 5) ||
+               (month == 6 && day <= 20);
+    }
+
+    public static void main(String[] args) {
+
+        int month = Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
+
+        if (isSpringSeason(month, day))
+            System.out.println("Its a Spring Season");
+        else
+            System.out.println("Not a Spring Season");
+    }
+}
