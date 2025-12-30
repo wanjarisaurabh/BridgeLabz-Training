239458a80fe4062f07ba80a748161e764696a967
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-30 13:03:22 +0530

    [saurabh wanjari ] Add . Added file HotelBooking

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/HotelBooking.java
@@ -0,0 +1,43 @@
+/*
+ * Program Name : HotelBooking
+ * Description  : Uses default, parameterized, and copy constructors
+ *                to manage hotel booking details.
+ */
+
+package com.constructor.level1;
+
+class HotelBooking {
+
+    // Booking details
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    // Parameterized constructor
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    // Method to display booking details
+    void display() {
+        System.out.println("Guest Name : " + guestName);
+        System.out.println("Room Type  : " + roomType);
+        System.out.println("Nights     : " + nights);
+    }
+}
