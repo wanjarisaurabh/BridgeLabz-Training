80440712e93587057711357927ba5e88c27df982
saurabh wanjari <wanjarisaurabh10@gmail.com>
2025-12-30 13:03:24 +0530

    [saurabh wanjari ] Add . Added file Student

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructor/level1/Student.java
@@ -0,0 +1,26 @@
+/*
+ * Program Name : Student
+ * Description  : Demonstrates access modifiers.
+ */
+
+package com.constructor.level1;
+
+class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
