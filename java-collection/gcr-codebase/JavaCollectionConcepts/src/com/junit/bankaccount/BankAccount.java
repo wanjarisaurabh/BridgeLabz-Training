0719573502ab61b437bfed204ee2beb7efd3c5b1
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-01-27 02:27:46 +0530

    [saurabh wanjari ] Add . Added  file of BankAccount

---
+++ java-collection/gcr-codebase/JavaCollectionConcepts/src/com/junit/bankaccount/BankAccount.java
@@ -0,0 +1,23 @@
+package com.junit.bankaccount;
+
+public class BankAccount {
+    private double balance = 0.0;
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Insufficient funds or invalid amount");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
