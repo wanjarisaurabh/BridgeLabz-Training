6dedd5bb7dfcd98666b7a912db5eb93461e28b76
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-01-27 02:32:32 +0530

    Add . Added file of ListManager

---
+++ java-collection/gcr-codebase/JavaCollectionConcepts/src/com/junit/listmanager/ListManager.java
@@ -0,0 +1,17 @@
+package com.junit.listmanager;
+
+import java.util.List;
+
+public class ListManager {
+    public void addElement(List<Integer> list, int element) {
+        list.add(element);
+    }
+
+    public void removeElement(List<Integer> list, int element) {
+        list.remove(Integer.valueOf(element));
+    }
+
+    public int getSize(List<Integer> list) {
+        return list.size();
+    }
+}
