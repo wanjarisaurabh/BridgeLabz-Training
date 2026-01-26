527d277cc2adc41d64e56dbecd970616b3f79692
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-01-27 02:32:33 +0530

    Add . Added file of TemperatureConverter

---
+++ java-collection/gcr-codebase/JavaCollectionConcepts/src/com/junit/temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package com.junit.temperatureconverter;
+
+public class TemperatureConverter {
+    
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
