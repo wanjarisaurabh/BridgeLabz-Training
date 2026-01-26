bbdfd0aa3e32bd41b54443fdd5f3db52c07ab9d3
saurabh wanjari <wanjarisaurabh10@gmail.com>
2026-01-27 02:32:32 +0530

    Add . Added file of ExceptionHandling

---
+++ java-collection/gcr-codebase/JavaCollectionConcepts/src/com/junit/exceptionhandling/MathUtils.java
@@ -0,0 +1,10 @@
+package com.junit.exceptionhandling;
+
+public class MathUtils {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
